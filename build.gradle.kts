039d56995cdcb9c3550d3df72e07aa9532f7da51
rico <kmirova731@gmail.com>
2025-06-11 00:21:49 +0500

    homework #1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,18 @@
 plugins {
-    kotlin("jvm") version "2.1.20"
+    kotlin("jvm") apply false
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+    group = rootProject.group
+    version = rootProject.version
 }
